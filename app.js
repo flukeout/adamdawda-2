764f5343ab89be1d07624d50302d8b92d77cf35a
Luke Pacholski <luke.pacholski@gmail.com>
2016-04-04 18:16:13 -0700

    some minor updates

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var stories = {
     "description" : "This and that and this and that, also this and that and this and that and also. That.",
     "descriptionPosition" : 5,
     "photos" : [
-      "alex/Alex1.jpg",
-      "alex/Alex2.jpg",
-      "alex/Alex3.jpg",
-      "alex/Alex4.jpg",
-      "alex/Alex5.jpg",
-      "alex/Alex6.jpg"
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg"
     ]
   },
   "balex" : {
@@ -19,23 +19,45 @@ var stories = {
     "description" : "This and that and this and that, also this and that and this and that and also. That.",
     "descriptionPosition" : 2,
     "photos" : [
-      "alex/Alex1.jpg",
-      "alex/Alex4.jpg",
-      "alex/Alex5.jpg",
-      "alex/Alex6.jpg"
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg"
+    ]
+  },
+  "malex" : {
+    "title" : "YOYO the skater",
+    "model" : "blammer",
+    "description" : "This and that and this and that, also this and that and this and that and also. That.",
+    "descriptionPosition" : 2,
+    "photos" : [
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg",
+      "alex/alex-small.jpg"
     ]
   }
 }
 
+var popupOpen = false;
+
 $(document).on("ready",function(){
   loadStories();
 
-  // openViewer("alex","photos/alex/Alex1.jpg");
+  $(window).on("scroll",function(e){
+    if(popupOpen){
+      e.preventDefault();
+    }
+  });
 
   $("body").on("click",".story img",function(){
     var image = $(this).attr("src");
     var story = $(this).closest(".story").attr("story");
     openViewer(story,image);
+    $("body").addClass("no-scroll");
+    popupOpen = true;
   });
 
   $("body").on("click",".popup-viewer .next-photo, .popup-viewer .previous-photo",function(){
@@ -55,10 +77,25 @@ $(document).on("ready",function(){
 
 
 function closePopup(){
+  $("body").removeClass("no-scroll");
   $(".popup-viewer").addClass("close");
   setTimeout(function(){
     $(".popup-viewer").hide();
   },300)
+}
+
+function sizeStory(el){
+  var totalWidth = 0;
+  var maxWidth = 90;
+
+  el.find(".photo-wrapper").each(function(){
+    totalWidth = $(this).width() + totalWidth;
+  });
+
+  el.find(".photo-wrapper").each(function(){
+    var percent = $(this).width() / totalWidth;
+    $(this).width(percent * 90 + "%");
+  });
 }
 
 function loadStories(){
@@ -69,17 +106,42 @@ function loadStories(){
     var storyEl = $("<section class='story'/>");
     storyEl.attr("story",k);
 
+    storyEl.attr("total",photos.length);
+    storyEl.attr("loaded",0);
+
+    var totalWidth = 90;
+
+
     for(var i = 0; i < photos.length; i++){
       var photo = photos[i];
       var photoEl = $("<div class='photo-wrapper'/>");
+      photoEl.css("width","10%");
       var imageEl = $("<img />");
+      imageEl.css("display","none");
       imageEl.attr("src","photos/" + photo);
+
+      imageEl.on("load",function(){
+        $(this).show();
+        var ratio = $(this).width() / $(this).height();
+        $(this).parent().css("width", 10 * ratio + "%");
+        var loaded = parseInt($(this).closest(".story").attr("loaded"));
+        var total = parseInt($(this).closest(".story").attr("total"));
+        loaded++;
+        $(this).closest(".story").attr("loaded",loaded);
+
+        if(loaded == total){
+          sizeStory($(this).closest(".story"));
+        }
+
+      });
+
       photoEl.append(imageEl);
       storyEl.append(photoEl);
     }
 
     if(story.description) {
       var photoEl = $("<div class='photo-wrapper story-description'/>");
+        photoEl.css("width","10%");
       var heading = $("<h1>" + story.title + "</h1>");
       var description = $("<p>" + story.description + "</p>");
       photoEl.append(heading).append(description);
@@ -105,12 +167,8 @@ function openViewer(storyName,image){
   viewer.find("h1").text(story.title);
   viewer.find("p").text(story.description);
   viewer.find(".image img").attr("src",image);
-
-  $(".popup-viewer").show("close");
+  $(".popup-viewer").css("display","block");
   $(".popup-viewer").removeClass("close");
-
-
-
 }
 
 function changeImage(direction){
